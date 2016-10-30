19b622550103003f8fec9c6db6c724fc57c837fc
sanderroosendaal <roosendaalsander@gmail.com>
2016-10-30 14:58:04 +0100

    Adding Subdirectory rowingphysics

---
+++ rowingphysics/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+
+import re
+
+def readme():
+    with open('README.rst') as f:
+	return f.read()
+
+setup(name='rowingphysics',
+
+      version=re.search(
+
+	  '^__version__\s*=\s*"(.*)"',
+	  open('rowingphysics/rowingphysics.py').read(),
+	  re.M
+
+	  ).group(1),
+
+      description='Rowing Physics calculations',
+      
+      long_description=readme(),
+
+      url='http://sanderroosendaal.wordpress.com',
+
+      author='Sander Roosendaal',
+
+      author_email='roosendaalsander@gmail.com',
+
+      license='MIT',
+
+#      py_modules = ['rowingphysics.crew',
+#		    'rowingphysics.rowingphysics',
+#		    'rowingphysics.rigging'],
+
+      packages=find_packages(),
+
+#      packages = ['rowingphysics','crew','rigging'],
+      
+      keywords = 'rowing ergometer concept2',
+      
+      install_requires=[
+	  'numpy',
+	  'scipy',
+	  'matplotlib',
+	  'pandas',
+	  ],
+
+      zip_safe=False,
+#      include_package_data=True,
+
+
+      )
