fdd9b59cdee60668f743e90e739d6b55f922e197
Sander Roosendaal <roosendaalsander@gmail.com>
2019-11-23 20:01:15 +0100

    some trial functions in go

--- rowingphysics/row_functions.py
+++ rowingphysics/row_functions.py
@@ -39,9 +39,9 @@ def drag_eq(displacement,velo,alfaref=3.5,doprint=0,constantdrag=0):
    rho = 999.97
 
    D = (displacement)/rho
-   Re = boatlength*velo/kinvis 
-   Cf = 0.075/((np.log10(Re)-2)**2)    
-   alpha = 0.5*rho*wetted_area*Cf    
+   Re = boatlength*velo/kinvis
+   Cf = 0.075/((np.log10(Re)-2)**2)
+   alpha = 0.5*rho*wetted_area*Cf
    alpha = alpha*corr  # correction, should be 1.0 normally
    a1 = alpha/0.8
 
@@ -58,7 +58,7 @@ def drag_eq(displacement,velo,alfaref=3.5,doprint=0,constantdrag=0):
       print(("alpha total : ",a1))
       print("----- Drag resistance data --------")
       print("")
-   
+
    W2 = a1*velo**2
 
    return W2
@@ -81,7 +81,7 @@ def d_recovery(dt,v,vc,dvc,mc,mb,alef):
    dvb = vboat(mc,mb,dvc)
 
    dv = dt*(-alef*(v-vb)**2)/(mb+mc)
-   
+
    return dv
 
 def d_stroke(dt,v,vc,dvc,mc,mb,alef,F):
@@ -90,34 +90,34 @@ def d_stroke(dt,v,vc,dvc,mc,mb,alef,F):
    dvb = vboat(mc,mb,dvc)
    Ftot = F - alef*(v-vb)**2
    dv = dt*Ftot/(mb+mc)
-   
+
    return dv
 
 def de_footboard(mc,mb,vs1,vs2):
    de = 0.0
    vt = 0.0
-   
+
    vb1 = vboat(mc,mb,vs1)
    vb2 = vboat(mc,mb,vs2)
 
-   vmb1 = vt-vb1   
+   vmb1 = vt-vb1
    vmb2 = vt-vb2
 
-   vmc1 = vt+vs1-vb1   
+   vmc1 = vt+vs1-vb1
    vmc2 = vt+vs2-vb2
-   
-   e_1 = 0.5*(mb*vmb1**2)+0.5*mc*vmc1**2 
-   e_2 = 0.5*(mb*vmb2**2)+0.5*mc*vmc2**2 
-   e_t = 0.5*(mc+mb)*vt**2 
+
+   e_1 = 0.5*(mb*vmb1**2)+0.5*mc*vmc1**2
+   e_2 = 0.5*(mb*vmb2**2)+0.5*mc*vmc2**2
+   e_t = 0.5*(mc+mb)*vt**2
 
 
-   de2 = e_2 - e_t 
-   de1 = e_1 - e_t 
+   de2 = e_2 - e_t
+   de1 = e_1 - e_t
 
    if (sign(vs1) == sign(vs2)):
-     de = max([e_2 - e_1,0]) 
+     de = max([e_2 - e_1,0])
    else:
-     de = max([e_2 - e_t,0]) 
+     de = max([e_2 - e_t,0])
 
    return de
 
@@ -136,7 +136,7 @@ def blade_force(oarangle,rigging,vb,fblade,doplot=0):
    up = vb*np.sin(oarangle)
    u = (u1**2 + up**2)**(0.5)  # fluid velocity
    a = np.arctan(u1/up)   # angle of attack
-  
+
    C_D = 2*CLmax*np.sin(a)**2.
    C_L = CLmax*np.sin(2.*a)
    area = rigging.bladearea
@@ -165,7 +165,7 @@ def blade_force(oarangle,rigging,vb,fblade,doplot=0):
          pyplot.show()
       except NameError:
          print("No plotting today")
-  
+
    vblade = phidot1*lout
 
    u1 = vblade-vb*np.cos(oarangle)
@@ -186,4 +186,3 @@ def blade_force(oarangle,rigging,vb,fblade,doplot=0):
    F_prop = F_R * np.cos(oarangle)
 
    return [phidot1,F_R,F_prop,F_L,F_D,C_L,C_D,a]
-
